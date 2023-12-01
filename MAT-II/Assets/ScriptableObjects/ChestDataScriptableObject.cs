2ea6b8c4851515b456331478e4f79069a248ca98
RagulPrasadG <gulshark572@gmail.com>
2023-12-01 11:48:00 +0530

    Scripts and UI initiated

---
+++ MAT-II/Assets/ScriptableObjects/ChestDataScriptableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewChestData",menuName = "Data/NewChestData")]
+public class ChestDataScriptableObject : ScriptableObject
+{
+   
+}
---
+++ MAT-II/Assets/Scripts/Utilities.cs
@@ -0,0 +1,15 @@
+public enum ChestStatus
+{
+    LOCKED,
+    UNLOCKING,
+    UNLOCKED,
+    COLLECTED
+}
+
+public enum ChestType
+{
+    COMMON,
+    RARE,
+    EPIC,
+    LEGENDARY
+}
